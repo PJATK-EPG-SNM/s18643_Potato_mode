061b20d9455cb78354e6470350d57e808dd127af
s18643-pja <61684678+s18643-pja@users.noreply.github.com>
2020-03-09 11:17:31 +0100

    v0.01
    gra :))

---
+++ Potato_Unity/Assets/Skwer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skwer : MonoBehaviour {
+
+    public float speed = 100;
+    public float jump = 550.3f;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        isGrounded = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xDisplacement = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        rb.velocity = new Vector2(xDisplacement, rb.velocity.y);
+
+        if (Input.GetKeyDown("space") && (isGrounded == true))
+        {
+            rb.AddForce(new Vector2(0, jump));
+            isGrounded = false;
+        }
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        isGrounded = true;
+    }
+}
